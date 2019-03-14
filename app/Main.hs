80d2d21ddfe4c0a38296ac2bbde74a6db7dbfbf9
Jacek Olczyk <jacek.olczyk98@gmail.com>
2019-03-14 17:48:24 +0100

    Implement the rest.

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,17 @@
 module Main where
 
-import           Data.List          (intersperse)
-import           Lib
+import           Control.Monad
+import           Data.List
+import           Data.Maybe         (isNothing)
+import           GHC.IO             (unsafePerformIO)
 import           System.Environment (getArgs, getProgName)
 import           System.IO
 import           Text.Read
 
--- | Functions used by part B
+import           Lib
+import           Mon
 
+-- | Functions used by part B
 prologue = "300 400 translate\n"
 
 epilogue = "stroke showpage\n"
@@ -20,16 +24,88 @@ pointToPostScript p =
   let ~(~(x1, y1), ~(x2, y2)) = pmap (pmap show) p
    in unwords [x1, y1, "moveto", x2, y2, "lineto\n"]
 
-showPicture :: IntRendering -> Either String String
-showPicture p = Right $ prologue ++ concatMap pointToPostScript p ++ epilogue
+showPicture :: IntRendering -> String
+showPicture = concatMap pointToPostScript
+
+addProEpi :: String -> String
+addProEpi x = prologue ++ x ++ epilogue
 
 -- Functions used by part C
+errorMessage = "/Courier findfont 24 scalefont setfont 0 0 moveto (Error) show\n"
 
-getPicture :: IO Picture
-getPicture = return $ rectangle 3 4
+passToAll :: (a -> b -> c -> d -> e -> f) -> f -> (f -> a) -> (f -> b) -> (f -> c) -> (f -> d) -> (f -> e) -> f
+passToAll f st a b c d e = f (a st) (b st) (c st) (d st) (e st)
+
+data PictureState = PictureState
+  { stack     :: [R]
+  , pic       :: [Line]
+  , pathStart :: Maybe R2
+  , trans     :: Transform
+  , curPoint  :: Maybe R2
+  }
+
+updateStack :: [R] -> PictureState -> PictureState
+updateStack newStack state = passToAll PictureState state (const newStack) pic pathStart trans curPoint
+
+updatePic :: [Line] -> PictureState -> PictureState
+updatePic newPic state = passToAll PictureState state stack (const newPic) pathStart trans curPoint
+
+updatePathStart :: Maybe R2 -> PictureState -> PictureState
+updatePathStart newPathStart state = passToAll PictureState state stack pic (const newPathStart) trans curPoint
+
+updateTrans :: Transform -> PictureState -> PictureState
+updateTrans newTransform state = passToAll PictureState state stack pic pathStart (const newTransform) curPoint
+
+updateCurPoint :: Maybe R2 -> PictureState -> PictureState
+updateCurPoint newCurPoint state = passToAll PictureState state stack pic pathStart trans (const newCurPoint)
+
+startState :: PictureState
+startState = PictureState [] [] Nothing m1 Nothing
+
+parsePicture :: [String] -> Maybe Picture
+parsePicture l = Picture . reverse . pic <$> foldl (\s el -> s >>= processLexeme el) (Just startState) l
+
+processLexeme :: String -> PictureState -> Maybe PictureState
+processLexeme l state
+  | l == "moveto", y:x:xs <- stack = Just
+    $ updateStack xs
+    $ updatePathStart (Just (x, y))
+    $ updateCurPoint (Just (x, y)) state
+  | l == "lineto", y:x:xs <- stack, Just cPoint <- currentPoint = Just
+    $ updateStack xs
+    $ updatePic (pmap (trR2 transforms) (cPoint, (x, y)) : pic)
+    $ updateCurPoint (Just (x, y)) state
+  | l == "closepath", Just start <- pathStart, Just cPoint <- currentPoint = Just
+    $ updatePic (pmap (trR2 transforms) (cPoint, start) : pic)
+    $ updateCurPoint (Just start) state
+  | l == "closepath" = Just state
+  | l == "translate", y:x:xs <- stack = Just
+    $ updateStack xs
+    $ updateTrans (translate (vec (x, y)) >< transforms) state
+  | l == "rotate", x:xs <- stack = Just
+    $ updateStack xs
+    $ updateTrans (rotate x >< transforms) state
+  | l == "add", x1:x2:xs <- stack = Just $ updateStack (x1 + x2 : xs) state
+  | l == "sub", x1:x2:xs <- stack = Just $ updateStack (x2 - x1 : xs) state
+  | l == "mul", x1:x2:xs <- stack = Just $ updateStack (x2 * x1 : xs) state
+  | l == "div", x1:x2:xs <- stack, x1 /= 0 = Just $ updateStack (x2 / x1 : xs) state
+  | Just x <- readMaybe l :: Maybe Int = Just $ updateStack (fromIntegral x : stack) state
+  | otherwise = Nothing
+  where
+    PictureState stack pic pathStart transforms currentPoint = state
+
+getPicture :: IO (Maybe Picture)
+getPicture = parsePicture . words <$> getContents
+
+maybeToEither :: String -> Maybe a -> Either String a
+maybeToEither s Nothing  = Left s
+maybeToEither _ (Just x) = Right x
+
+getEither :: Either a a -> a
+getEither (Left x)  = x
+getEither (Right x) = x
 
 -- The main function.
-
 main :: IO ()
 main = do
   programName <- getProgName
@@ -39,5 +115,4 @@ main = do
         | length args == 1 = readEither $ head args
         | otherwise = Left $ helpMessage programName
   p <- getPicture
-  let str = getEither $ (flip renderScaled p <$> n) >>= showPicture
-  putStrLn str
+  putStrLn $ addProEpi $ getEither $ fmap showPicture $ renderScaled <$> n <*> maybeToEither errorMessage p
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,106 +1,89 @@
-{-# LANGUAGE FlexibleInstances    #-}
-{-# LANGUAGE TupleSections        #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TupleSections #-}
 {-# LANGUAGE TypeSynonymInstances #-}
 
-module Lib where
+module Lib
+  ( R
+  , R2
+  , Vec
+  , Point
+  , point
+  , vec
+  , Picture(..)
+  , line
+  , rectangle
+  , (&)
+  , IntLine
+  , IntRendering
+  , renderScaled
+  , Transform
+  , translate
+  , rotate
+  , fullCircle
+  , trpoint
+  , trvec
+  , transform
+  -- also export some utils
+  , pmap
+  , trR2
+  , Line
+  ) where
 
-import           Control.Monad
-import           Data.Fixed    (mod')
-import           GHC.Real
-import           Mon
-import           Prelude       hiding (cos, sin)
+import Control.Monad
+import Data.Fixed (mod')
+import GHC.IO (unsafePerformIO)
+import GHC.Real
+import Mon
+import Prelude hiding (cos, sin)
 
 -- | All the type definitions.
 type R = Rational
 
 type R2 = (R, R)
 
-type Point = R2
+newtype Point = Point
+  { getCoords :: R2
+  } deriving (Show, Eq)
 
-newtype Vec =
-  Vec R2
-  deriving (Show, Eq)
+newtype Vec = Vec
+  { getVec :: R2
+  } deriving (Show, Eq)
 
 newtype Transform =
   Trans (Vec, R)
   deriving (Show, Eq)
 
-type Picture = [(Point, Point)]
+type Line = (R2, R2)
+
+newtype Picture = Picture
+  { getLineList :: [Line]
+  } deriving (Show, Eq)
 
 type IntLine = ((Int, Int), (Int, Int))
 
 type IntRendering = [IntLine]
 
 -- | All the instances.
-instance Num Vec where
-  Vec (x1, y1) + Vec (x2, y2) = Vec (x1 + x2, y1 + y2)
-  (-) = undefined
-  (*) = undefined
-  abs = undefined
-  signum = undefined
-  fromInteger = undefined
-
 instance Mon Vec where
   m1 = Vec (0, 0)
-  x >< y = x + y
+  x >< y = vec (fx + sx, fy + sy)
+    where
+      (fx, fy) = getVec x
+      (sx, sy) = getVec y
 
 instance Mon Transform where
   m1 = Trans (Vec (0, 0), 0)
-  Trans (Vec (x1, y1), a1) >< Trans (Vec (x2, y2), a2) = Trans (Vec (x1 + x2, y1 + y2), (a1 + a2) `mod'` 360)
+  x >< y = Trans (Vec (x1 + x2, y1 + y2), (a1 + a2) `mod'` 360)
+    where
+      Trans (Vec (x1, y1), a1) = x
+      Trans (Vec (x2, y2), a2) = y
 
 -- | Helper functions that are not directly part of the solution.
 pmap :: (a -> b) -> (a, a) -> (b, b)
 pmap f (x, y) = (f x, f y)
 
-lift :: (b -> b -> c) -> (a -> b) -> a -> a -> c
-lift f g x1 x2 = f (g x1) (g x2)
-
-getEither :: Either a a -> a
-getEither (Left x)  = x
-getEither (Right x) = x
-
--- | Functions that are part of the solution
-vec :: R2 -> Vec
-vec = Vec
-
-point :: R2 -> Point
-point = id
-
--- odcinek pomiędzy punktami o podanych współrzędnych
-line :: (R, R) -> (R, R) -> Picture
-line x y = [(point x, point y)]
-
--- prostokąt o podanej szerokości i wysokości zaczepiony w (0,0)
-rectangle :: R -> R -> Picture
-rectangle w h =
-  [ (point (0, 0), point (w, 0))
-  , (point (w, 0), point (w, h))
-  , (point (w, h), point (0, h))
-  , (point (0, h), point (0, 0))
-  ]
-
-(&) :: Picture -> Picture -> Picture
-(&) = (++)
-
--- Obrazowanie przy danym współczynniku powiększenia
--- z zaokrągleniem do najbliższych wartości całkowitych
-renderScaled :: Int -> Picture -> IntRendering
-renderScaled x p = _render $ _scale x p
-  where
-    _scale fac p = p
-    _render = map $ pmap $ pmap round
-
--- przesunięcie o wektor
-translate :: Vec -> Transform
-translate = Trans . (, 0)
-
--- obrót wokół punktu (0,0) przeciwnie do ruchu wskazówek zegara
--- jednostki mozna sobie wybrać
-rotate :: R -> Transform
-rotate = Trans . (m1, ) . (`mod'` 360)
-
-fullCircle :: R -- wartość odpowiadająca 1 pełnemu obrotowi (360 stopni)
-fullCircle = 0
+applyBoth :: (b -> b -> c) -> (a -> b) -> a -> a -> c
+applyBoth f g x1 x2 = f (g x1) (g x2)
 
 cos :: Rational -> Rational
 sin x' = (4 * x * (180 - x)) / (40500 - x * (180 - x))
@@ -110,16 +93,50 @@ sin x' = (4 * x * (180 - x)) / (40500 - x * (180 - x))
 sin :: Rational -> Rational
 cos = sin . (90 +)
 
-trpoint :: Transform -> Point -> Point
-trpoint (Trans (Vec (x, y), a)) (px, py) = _doRotate a (x + px, y + py)
+trR2 :: Transform -> R2 -> R2
+trR2 (Trans (Vec (x, y), a)) (px, py) = _doRotate a (x + px, y + py)
   where
     _doRotate a ~(x, y) = (x * c - y * s, x * s + y * c)
       where
         c = cos a
         s = sin a
 
+-- | Functions that are part of the solution
+vec :: R2 -> Vec
+vec = Vec
+
+point :: R2 -> Point
+point = Point
+
+line :: (R, R) -> (R, R) -> Picture
+line x y = Picture [(x, y)]
+
+rectangle :: R -> R -> Picture
+rectangle w h = Picture [((0, 0), (w, 0)), ((w, 0), (w, h)), ((w, h), (0, h)), ((0, h), (0, 0))]
+
+(&) :: Picture -> Picture -> Picture
+x & y = Picture $ getLineList x ++ getLineList y
+
+renderScaled :: Int -> Picture -> IntRendering
+renderScaled x p = _render $ _scale x (getLineList p)
+  where
+    _scale fac = map $ pmap (pmap (* fromIntegral fac))
+    _render = map $ pmap $ pmap round
+
+translate :: Vec -> Transform
+translate = Trans . (, 0)
+
+rotate :: R -> Transform
+rotate = Trans . (m1, ) . (`mod'` 360)
+
+fullCircle :: R
+fullCircle = 0
+
+trpoint :: Transform -> Point -> Point
+trpoint t = point . trR2 t . getCoords
+
 trvec :: Transform -> Vec -> Vec
-trvec t (Vec p) = Vec $ trpoint t p
+trvec t p = Vec $ getCoords $ trpoint t (point $ getVec p)
 
 transform :: Transform -> Picture -> Picture
-transform tr = map $ pmap $ trpoint tr
+transform tr p = Picture $ map (pmap $ trR2 tr) (getLineList p)
